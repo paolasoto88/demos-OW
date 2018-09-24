f6b60f7c340bfaa73bbfbfd2726bc1ddaf5d5fa7
paolasoto88 <paolasoto88@gmail.com>
2018-09-24 14:19:55 +0200

    identation error solved

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
- def main(args):
-     name = args.get("name", "stranger")
-     greeting = "Hello " + name + "!"
-     print(greeting)
-     return {"greeting": greeting}
+def main(args):
+	name = args.get("name", "stranger")
+	greeting = "Hello " + name + "!"
+	print(greeting)
+	return {"greeting": greeting}
